f8fab1898bdb76bcb4fabaea8ce3b4d98eaab49a
Salla0905 <bruna.salla@germinare.org.br>
2024-10-28 09:47:12 -0300

    subindo a classe BuscarPlano, que não tinha ido anteriormente

---
+++ ServletViajou/src/main/java/com/example/servletviajou/Controller/BuscarPlano.java
@@ -0,0 +1,41 @@
+package com.example.servletviajou.Controller;
+
+import com.example.servletviajou.DAO.PlanoDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class BuscarPlano extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+
+        PlanoDAO planoDAO = new PlanoDAO();
+
+        try{
+            int id = Integer.parseInt(request.getParameter("id"));
+            planoDAO.buscar(id);
+            if(planoDAO.buscar(id) != null){
+                request.setAttribute("plano", planoDAO.buscar(id));
+                request.getRequestDispatcher("/WEB-INF/views/excursaoDetalhes.jsp").forward(request, response);
+            }
+        }
+        catch (NumberFormatException e) {
+            request.setAttribute("erro", "ID inválido!");
+            request.getRequestDispatcher("/WEB-INF/views/erro.jsp").forward(request, response);
+        }
+        catch (Exception e) {
+            request.setAttribute("erro", "Erro ao buscar a excursão.");
+            request.getRequestDispatcher("/WEB-INF/views/erro.jsp").forward(request, response);
+        }
+    }
+}
