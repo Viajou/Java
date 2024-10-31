3e0d90be9c720b8c31c9da0ed4dcfbd8f841c3dd
Brenosilva13 <breno.souza@germinare.org.br>
2024-10-31 12:22:35 -0300

    colocando jdbc

--- ServletViajou/src/main/java/com/example/servletviajou/Conexao.java
+++ ServletViajou/src/main/java/com/example/servletviajou/Conexao.java
@@ -19,34 +19,32 @@ public class Conexao {
     String user = dotenv.get("db_usuario");
     String password = dotenv.get("db_senha");
 
-    public boolean conectar(){
-        try{
+    public boolean conectar() {
+        try {
 //          informando qual driver será utilizado para a conexão
             Class.forName("org.postgresql.Driver");
             //fazendo a conexão
             conn = DriverManager.getConnection(url, user, password);
             return true;
-        }
-        catch (SQLException sqle){
-            System.out.println("Servidor não encontrado\n"+sqle);
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println("Driver não encontrado\n" + cnfe);
             return false;
-        }
-        catch (ClassNotFoundException cnfe){
-            System.out.println("Classe não encontrada\n"+cnfe);
-            return false;
-        }
-    }
-
-    public boolean desconectar(){
-        try {
-            if (conn != null && !conn.isClosed()) {
-                //Desconectando BD
-                conn.close();
-            }
-            return true;
-        }catch (SQLException sqle) {
+        } catch (SQLException sqle) {
             System.out.println("Servidor não encontrado\n" + sqle);
             return false;
         }
     }
+
+        public boolean desconectar() {
+            try {
+                if (conn != null && !conn.isClosed()) {
+                    //Desconectando BD
+                    conn.close();
+                }
+                return true;
+            } catch (SQLException sqle) {
+                System.out.println("Servidor não encontrado\n" + sqle);
+                return false;
+            }
+        }
 }
