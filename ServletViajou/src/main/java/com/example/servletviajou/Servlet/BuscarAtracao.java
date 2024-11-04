4ff9292494bfc70f2946caba56b7f4a0c130190a
Brenosilva13 <breno.souza@germinare.org.br>
2024-11-04 08:21:45 -0300

    arrumando merge

--- ServletViajou/src/main/java/com/example/servletviajou/Servlet/BuscarAtracao.java
+++ ServletViajou/src/main/java/com/example/servletviajou/Servlet/BuscarAtracao.java
@@ -10,7 +10,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-<<<<<<< HEAD
 
 import com.example.servletviajou.DAO.PlanoDAO;
 import jakarta.servlet.ServletException;
@@ -30,25 +29,10 @@ public class BuscarAtracao extends HttpServlet {
 
         int atracaoId = Integer.parseInt(search);
         ResultSet busca = atracaoDAO.buscar(atracaoId);
-=======
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-@WebServlet(name = "BuscarAtracao" , value = "/BuscarAtracao-servelt")
-public class BuscarAtracao extends HttpServlet {
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        String search = request.getParameter("search");
-        AtracaoDAO atracaoDAO = new AtracaoDAO();
-        int adminId;
-        adminId = Integer.parseInt(search);
-        ResultSet busca = atracaoDAO.buscar(adminId);
->>>>>>> a8df654a7766af2c0a55e9e63311d269c4765cf7
 
         try {
             // Verifica se o ResultSet está vazio
             if (busca.next()) { // Se não há resultados
-<<<<<<< HEAD
                 ResultSet certo = atracaoDAO.buscar(atracaoId);
                 request.setAttribute("resultados", certo);
                 // Redireciona para a página de listagem
@@ -58,28 +42,12 @@ public class BuscarAtracao extends HttpServlet {
                 request.getRequestDispatcher("listar_atracao.jsp").forward(request, response);
             }
 
-
         } catch (NumberFormatException nfe) {
             // Trata o caso onde o ID não é um número válido
             request.setAttribute("errorMessage", "Por favor, insira um ID válido.");
             request.getRequestDispatcher("listar_plano.jsp").forward(request, response);
-=======
-                ResultSet certo = atracaoDAO.buscar(adminId);
-                request.setAttribute("resultados", certo);
-                // Redireciona para a página de listagem
-                request.getRequestDispatcher("listar_atracao.jsp").forward(request, response);
-            }
-            else {
-                request.setAttribute("naoEncontrado", "Admin não encontrado...");
-                request.getRequestDispatcher("listar_admin.jsp").forward(request, response);
-            }
-        } catch (NumberFormatException nfe) {
-            // Trata o caso onde o ID não é um número válido
-            request.setAttribute("errorMessage", "Por favor, insira um ID válido.");
-            request.getRequestDispatcher("listar_admin.jsp").forward(request, response);
->>>>>>> a8df654a7766af2c0a55e9e63311d269c4765cf7
-        }catch (SQLException sqle) {
-            request.setAttribute("errorMessage", sqle.getMessage());
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
         }
     }
 }
